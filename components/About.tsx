16179d4e092da69462673062ed3619efe203a17d
SpaceXEngineer <148818352+FalconDevX@users.noreply.github.com>
2025-11-27 04:00:33 +0100

    Refactor About component layout for improved styling and responsiveness. Adjust gap and alignment properties for better visual presentation on different screen sizes.

--- components/About.tsx
+++ components/About.tsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div id="about" className="appear w-full min-h-[60vh] flex flex-col items-center scroll-mt-15">
       <h1 className="text-2xl p-6">About Us</h1>
-      <div className="flex flex-col md:flex-row gap-5">
-        <div className="w-full md:w-1/2 px-5">
+      <div className="flex flex-col md:flex-row gap-8 justify-center max-w-6xl mx-auto">
+        <div className="w-full md:w-1/2 px-5 flex justify-center md:justify-end">
           <p className="text-white text-md max-w-xl">AGH Analytica is a newly established student organization at AGH University of Science and Technology, bringing together enthusiasts of data analysis and cutting-edge technologies. <br /> <br /> Our activities focus on innovative projects that allow members to develop their skills in satellite imagery and map analysis, stock market data analysis, image analysis and working with Large Language Models (LLMs).</p>
         </div>
         <div className="w-full md:w-1/2 px-5">
